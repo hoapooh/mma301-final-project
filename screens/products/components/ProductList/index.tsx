622a95f30037892988135e853e06eb18b2a84047
Viet Hung <viethung01234@gmail.com>
2025-02-27 20:06:23 +0700

    fix: product list layout

--- screens/products/components/ProductList/index.tsx
+++ screens/products/components/ProductList/index.tsx
@@ -10,7 +10,6 @@ import getProductPrice from '../../utils/getProductPrice';
 import { Link } from 'expo-router';
 import { Grid, GridItem } from '@/components/ui/grid';
 
-
 const ProductListItem: React.FC<{ product: IProduct }> = ({ product }) => {
   return (
     <Card className=" rounded-lg w-full border-red-100">
@@ -32,9 +31,9 @@ const ProductListItem: React.FC<{ product: IProduct }> = ({ product }) => {
         />
 
         <VStack className="mb-6">
-        <Text size="sm">{product.title}</Text>
+          <Text size="sm">{product.title}</Text>
           <Heading size="md" className="mb-4">
-          ${getProductPrice(product)}
+            ${getProductPrice(product)}
           </Heading>
         </VStack>
       </Link>
@@ -54,22 +53,24 @@ const ProductList: React.FC<ProductListProps> = (props) => {
   }
 
   return (
-    <ScrollView className="h-full px-2">
+    <ScrollView className="h-full">
       <Grid
-        className="gap-5"
+        className="gap-2"
         _extra={{
-          className: 'grid-cols-2'
+          className: 'grid-cols-12',
         }}
       >
         {props.data.map((product) => (
-          <GridItem key={product.id} _extra={{
-            className: "col-span-1"
-          }}>
+          <GridItem
+            key={product.id}
+            _extra={{
+              className: 'col-span-6',
+            }}
+          >
             <ProductListItem product={product} />
           </GridItem>
         ))}
       </Grid>
-      
     </ScrollView>
   );
 };
