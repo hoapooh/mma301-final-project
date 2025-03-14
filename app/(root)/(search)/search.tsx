5a23bcd8169bff5fd6df91e60db56be19212c948
Giang Nguyen Thi Huong <nguyenthihuonggiangthth@gmail.com>
2025-03-14 13:24:49 +0700

    Add sort

--- app/(root)/(search)/search.tsx
+++ app/(root)/(search)/search.tsx
@@ -3,7 +3,48 @@ import useProductList from '@/screens/products/hooks/useProductList';
 import { useNavigation } from 'expo-router';
 import { useLocalSearchParams } from 'expo-router/build/hooks';
 import { Text, View } from 'react-native';
-
+import {
+  Select,
+  SelectTrigger,
+  SelectInput,
+  SelectIcon,
+  SelectPortal,
+  SelectBackdrop,
+  SelectContent,
+  SelectDragIndicator,
+  SelectDragIndicatorWrapper,
+  SelectItem,
+} from "@/components/ui/select"
+import { ChevronDownIcon } from "@/components/ui/icon"
+const sortOptions = [
+  { key: "high-low", label: "Price: High-Low" },
+  { key: "low-high", label: "Price: Low-High" },
+  { key: "a-z", label: "Name: A-Z" },
+  { key: "z-a", label: "Name: Z-A" },
+];
+const MenuButton = ()=>{
+  return (
+    <View style={{ width: "100%", display: 'flex', justifyContent: 'flex-end', flexDirection:'row', padding: 10}}>
+    <Select>
+      <SelectTrigger variant="outline" size="md" className="w-full h-50" >
+        <SelectInput placeholder="Select option"/>
+        <SelectIcon className="mr-3" as={ChevronDownIcon} />
+      </SelectTrigger>
+      <SelectPortal>
+        <SelectBackdrop />
+        <SelectContent>
+          <SelectDragIndicatorWrapper>
+            <SelectDragIndicator />
+          </SelectDragIndicatorWrapper>
+          {sortOptions.map((option) => (
+              <SelectItem key={option.key} label={option.label} value={option.key} />
+            ))}
+        </SelectContent>
+      </SelectPortal>
+    </Select>
+    </View>
+  )
+}
 const SearchResult = () => {
   const params = useLocalSearchParams();
   const nav = useNavigation();
@@ -14,7 +55,8 @@ const SearchResult = () => {
   }
 
   return (
-    <View>
+    <View style={{ flex: 1 }}>
+      <MenuButton/>
       <ProductList data={query.data?.products} />
     </View>
   );
